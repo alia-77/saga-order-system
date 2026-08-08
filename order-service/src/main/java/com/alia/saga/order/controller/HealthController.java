ecd6dda79ee6b2678747885d68b83922a0891a97
alia-77 <aliaahmadamin@gmail.com>
2026-08-08 20:41:26 +0300

    Connect order controller to service layer

--- order-service/src/main/java/com/alia/saga/order/controller/HealthController.java
+++ order-service/src/main/java/com/alia/saga/order/controller/HealthController.java
@@ -1,14 +1,20 @@
 package com.alia.saga.order.controller;
 
+import com.alia.saga.order.service.OrderService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthController {
 
+    private final OrderService orderService;
+
+    public HealthController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
     @GetMapping("/")
     public String health() {
         return "Order Service is running!";
     }
-
 }
