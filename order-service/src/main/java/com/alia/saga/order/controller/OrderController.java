4c5311fb7f4f8aee04ad957cae7c5078da86fd55
alia-77 <aliaahmadamin@gmail.com>
2026-08-10 20:16:31 +0300

    Complete order REST API

---
+++ order-service/src/main/java/com/alia/saga/order/controller/OrderController.java
@@ -0,0 +1,33 @@
+package com.alia.saga.order.controller;
+
+import com.alia.saga.order.model.Order;
+import com.alia.saga.order.service.OrderService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @PostMapping
+    public Order createOrder(@RequestBody Order order) {
+        return orderService.createOrder(order);
+    }
+
+    @GetMapping
+    public List<Order> getAllOrders() {
+        return orderService.getAllOrders();
+    }
+
+    @GetMapping("/{id}")
+    public Order getOrderById(@PathVariable("id") Long id) {
+        return orderService.getOrderById(id);
+    }
+}
